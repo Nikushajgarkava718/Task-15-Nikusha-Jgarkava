abc7e140acbf333e3e48d5e54842f5b2bbfcb6aa
Nikushajgarkava45 <nikushajgarkava3@gmail.com>
2026-07-27 20:39:40 +0400

    js variants ( math things)

--- app.js
+++ app.js
@@ -18,8 +18,15 @@ let userRestrictions;
 
 // number
 let studentScore = 90;
-
-// console.log(typeof studentScore);
+const infinity1 = 1 / 0;
+const infinity2 = -1 / 0;
+console.log(infinity1, typeof infinity1);
+// console.log(5 + 10); // addition
+// console.log(5 - 10); // substraction
+// console.log(5 * 10); // multiplication
+// console.log(5 / 10); //division
+// console.log(5 ** 2); // power
+// console.log(5 % 10); // reminder
 
 //string
 let studentName = "Nikusha";
@@ -27,3 +34,28 @@ let studentLastName = "Jgarkava";
 let studentFullName = `${studentName} ${studentLastName}`; // template literals
 
 // console.log(typeof studentFullName);
+
+// boolean true false
+let isStudentVerified = true;
+// console.log(typeof isStudentVerified);
+
+//  bigint
+const bigNumber = 12341234123123123n;
+// console.log(typeof bigNumber);
+
+// symbol
+const id1 = Symbol("id");
+const id2 = Symbol("id");
+
+// console.log(typeof id1);
+
+// undefined
+let userVerified;
+
+// console.log(userVerified, typeof userVerified);
+userVerified = true;
+
+// null
+let productsList = null;
+
+// console.log(typeof productsList); //  object
