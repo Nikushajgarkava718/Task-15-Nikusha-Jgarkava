02a999ffa40c9ce6f3bfc308b8de8f53061bc77b
Nikushajgarkava45 <nikushajgarkava3@gmail.com>
2026-07-27 19:16:05 +0400

    using ${}

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("hello , yoko");
+// console.log("hello , yoko");
+
+let userName = "Nino";
+
+const minAge = 24;
+
+// console.log(minAge);
+
+// declaration
+let userEmail;
+
+// initialization
+userEmail = "test@gmail.com";
+
+//assign
+let userPassword = "test1234";
+let userRestrictions;
+
+// number
+let studentScore = 90;
+
+// console.log(typeof studentScore);
+
+//string
+let studentName = "Nikusha";
+let studentLastName = "Jgarkava";
+let studentFullName = `${studentName} ${studentLastName}`; // template literals
+
+// console.log(typeof studentFullName);
